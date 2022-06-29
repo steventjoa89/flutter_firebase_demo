d81fbc1e10d60700c95520f9d48850a4f9c6fc36
Steven Tjoa <steven.tjoa@allianz.co.id>
2022-06-29 18:47:57 +0700

    First init.

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Main App'),
+        ),
+        body: Center(
+          child: Text('Hello world'),
+        ),
+      ),
+    );
+  }
+}
